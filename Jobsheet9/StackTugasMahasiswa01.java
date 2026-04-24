b3deda6185a76b3cdb4dbad5fbaf7db57839087b
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2026-04-24 18:42:36 +0700

    Praktikum 2

--- Jobsheet9/StackTugasMahasiswa01.java
+++ Jobsheet9/StackTugasMahasiswa01.java
@@ -66,6 +66,19 @@ public class StackTugasMahasiswa01 {
     public int tugasDikumpulkan(){
         return top + 1;
     }
+    public String konversiDesimalkeBiner(int nilai){
+        StackKonversi01 stack = new StackKonversi01();
+        while (nilai > 0){
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()){
+            biner += stack.pop();
+        }
+        return biner;
+    }
     public void print(){
         for (int i = top; i >= 0; i--){
             System.out.println(stack[i].nama+"\t"+stack[i].nim+"\t"+stack[i].kelas);
